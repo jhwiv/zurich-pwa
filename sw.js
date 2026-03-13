7a6f67beea4cfceede13941f3d58c0665257f7c4
jhwiv <jhwiv.online@gmail.com>
2026-03-13 11:04:00 -0400

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,64 @@
+const CACHE = 'zurich-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;0,900;1,400&family=Crimson+Pro:ital,wght@0,300;0,400;0,600;1,300;1,400&family=DM+Mono:wght@300;400;500&display=swap'
+];
+
+// Install: cache everything
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      // Cache local assets reliably; fonts best-effort
+      return cache.addAll(['./index.html', './manifest.json'])
+        .then(() => cache.addAll(['./icon-192.png', './icon-512.png']).catch(() => {}))
+        .then(() => cache.add(ASSETS[ASSETS.length - 1]).catch(() => {}));
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// Activate: clean old caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Fetch: cache-first for local, network-first for external
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  const isLocal = url.origin === self.location.origin;
+
+  if (isLocal) {
+    // Cache-first strategy for local assets
+    e.respondWith(
+      caches.match(e.request).then(cached => {
+        if (cached) return cached;
+        return fetch(e.request).then(response => {
+          if (response.ok) {
+            const clone = response.clone();
+            caches.open(CACHE).then(cache => cache.put(e.request, clone));
+          }
+          return response;
+        }).catch(() => caches.match('./index.html'));
+      })
+    );
+  } else {
+    e.respondWith(
+      fetch(e.request)
+        .then(response => {
+          if (response.ok) {
+            const clone = response.clone();
+            caches.open(CACHE).then(cache => cache.put(e.request, clone));
+          }
+          return response;
+        })
+        .catch(() => caches.match(e.request))
+    );
+  }
+});
